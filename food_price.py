213ab52b59b618122b77a2991f4d7cfff8a6795f
sujal shresth poddar <99262906+sujal325@users.noreply.github.com>
2022-06-30 16:43:54 +0530

    Add files via upload

---
+++ food_price.py
@@ -0,0 +1,3 @@
+fruit_price={'apple':2.99,'orange':4.99,'pineapple':12.99}
+name_of_fruit=input('which fruit do want to choose apple,orange or pineapple: ')
+print(f'you pay {fruit_price[name_of_fruit]}')
